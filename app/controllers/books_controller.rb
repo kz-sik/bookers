2b17ca5418b16409db2d81a382ab5f30705a780d
EC2 Default User <ec2-user@ip-172-31-30-7.ap-northeast-1.compute.internal>
2022-10-13 02:47:37 +0000

    indexにtableタグ記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,16 +5,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
+    book = Book.new(book_params)
+    if book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(book.id)
     else
       render :index
     end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
